09534033e4844061561810c5ec34c5b4b895e754
BokiCode <bojancodedev@gmail.com>
2024-01-05 19:40:30 +0100

    added delete and edit button

--- js/script.js
+++ js/script.js
@@ -1,13 +1,32 @@
+//let tasksList = [];
 document.querySelector("#submitTaskButton").addEventListener("click", function () {
     const inputFieldText = document.querySelector("#inputField").value;
     if(inputFieldText === "") {
         alert("You didn't write anything!");
     } else {
-        let newTask = document.createElement("p")
-        newTask.classList.add("newTaskStylization")
-        newTask.innerText = inputFieldText;
-        document.querySelector("#taskHolder").appendChild(newTask);
+        let newTask = document.createElement("input")
+        newTask.classList.add("taskStylization")
+        newTask.setAttribute("value",inputFieldText);
+        newTask.disabled = true;
 
+        const deleteTaskButton = document.createElement("button")
+        deleteTaskButton.innerText = "del";
+        deleteTaskButton.addEventListener("click", function() {
+            document.querySelector("#taskHolder").removeChild(newTask);
+            document.querySelector("#taskHolder").removeChild(deleteTaskButton);
+            document.querySelector("#taskHolder").removeChild(editTaskButton);
+        });
+
+        const editTaskButton = document.createElement("button")
+        editTaskButton.innerText = "edit";
+        editTaskButton.addEventListener("click", function() {
+            newTask.disabled = !newTask.disabled;
+        });
+
+        document.querySelector("#taskHolder").appendChild(newTask);
+        document.querySelector("#taskHolder").appendChild(deleteTaskButton);
+        document.querySelector("#taskHolder").appendChild(editTaskButton);
+        //tasksList.push(inputFieldText);
     }
     document.querySelector("#inputField").value = "";
 })
