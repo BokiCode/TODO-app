98be321e81bb19db1d847efd11d75f2641a8b955
BokiCode <bojancodedev@gmail.com>
2024-01-06 14:24:13 +0100

    changed button size

--- js/script.js
+++ js/script.js
@@ -1,32 +1,32 @@
-//let tasksList = [];
 document.querySelector("#submitTaskButton").addEventListener("click", function () {
     const inputFieldText = document.querySelector("#inputField").value;
-    if(inputFieldText === "") {
+    if (inputFieldText === "") {
         alert("You didn't write anything!");
     } else {
         let newTask = document.createElement("input")
-        newTask.classList.add("taskStylization")
-        newTask.setAttribute("value",inputFieldText);
+        newTask.classList.add("inputField");
+        newTask.setAttribute("value", inputFieldText);
         newTask.disabled = true;
 
-        const deleteTaskButton = document.createElement("button")
+        const deleteTaskButton = document.createElement("button");
         deleteTaskButton.innerText = "del";
-        deleteTaskButton.addEventListener("click", function() {
+        deleteTaskButton.classList.add("button");
+        deleteTaskButton.addEventListener("click", function () {
             document.querySelector("#taskHolder").removeChild(newTask);
             document.querySelector("#taskHolder").removeChild(deleteTaskButton);
             document.querySelector("#taskHolder").removeChild(editTaskButton);
         });
 
-        const editTaskButton = document.createElement("button")
+        const editTaskButton = document.createElement("button");
         editTaskButton.innerText = "edit";
-        editTaskButton.addEventListener("click", function() {
+        editTaskButton.classList.add("button");
+        editTaskButton.addEventListener("click", function () {
             newTask.disabled = !newTask.disabled;
         });
 
         document.querySelector("#taskHolder").appendChild(newTask);
         document.querySelector("#taskHolder").appendChild(deleteTaskButton);
         document.querySelector("#taskHolder").appendChild(editTaskButton);
-        //tasksList.push(inputFieldText);
     }
     document.querySelector("#inputField").value = "";
 })
